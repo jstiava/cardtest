ec2e3638038ac8254146439e92d57e549e679be3
jstiava <77993933+jstiava@users.noreply.github.com>
2023-04-14 11:22:43 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer>
-     <a href=”https://wustl.edu/” “text-decoration: none”>
+     <a href=”https://wustl.edu/” style=“text-decoration: none”>
           <img id="footer_shield" src="<?php echo esc_url( get_template_directory_uri() . '/icons/washu_digital.svg' ); ?>" alt="Washington University in St. Louis">
      </a>
      <h6>Campus Card Services</h6>
@@ -7,7 +7,7 @@
      <br>
      <p>One Brookings Drive, St. Louis, MO</p>
      <p>Ann W. Olin Women's Building, Suite 002</p>
-     <a href="">Contact Us</a>
+     <a href="https://card.wustl.edu/contact-us/">Contact Us</a>
 </footer>
 <?php wp_footer(); ?>
 </body>
